a4dc0710f8057fe4fbbe80112d435b299fb497b0
scwllj <33397888+scwllj@users.noreply.github.com>
2017-11-10 14:23:35 +0800

    1

--- app/src/main/java/mianfeiwaptoapp/zhuanqian/com/mianfeiwaptoapp/MyApplication.java
+++ app/src/main/java/mianfeiwaptoapp/zhuanqian/com/mianfeiwaptoapp/MyApplication.java
@@ -2,6 +2,8 @@ package mianfeiwaptoapp.zhuanqian.com.mianfeiwaptoapp;
 
 import android.app.Application;
 
+import com.google.android.gms.ads.MobileAds;
+
 /**
  * Created by 李宏阳 on 2017/11/9.
  */
@@ -10,5 +12,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+// Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
     }
 }
